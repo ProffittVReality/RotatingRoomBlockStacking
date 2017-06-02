6bde64fe9d7b9fb57086aeaeb13c05b0a065926d
Proffitt <uvaperceptionlab@gmail.com>
2017-06-02 12:46:42 -0400

    First Committ
    Room rotates at a constant speed while user stacks blocks. User states
    whether or not room was rotating after completing each stack

---
+++ Rotating Room Study Gain/Assets/Calculations.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Calculations : MonoBehaviour {
+
+	private double angle;
+	private double dX;
+	private double dZ;
+	private double count;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public int sort(Vector3 vect) {
+
+		if (vect.x > 0 && vect.z > 0)
+			return 1;
+		else if (vect.x < 0 && vect.z > 0)
+			return 2;
+		else if (vect.x < 0 && vect.z < 0)
+			return 3;
+		else if (vect.x > 0 && vect.z < 0)
+			return 4;
+		else
+			return 0;
+	}
+
+	public double calc(Vector3 vect, Vector3 vect2) {
+		double dX1 = Math.Abs (vect.x);
+		double dZ1 = Math.Abs (vect.z);
+		double dX2 = Math.Abs (vect2.x);
+		double dZ2 = Math.Abs (vect2.z);
+
+		//count
+		count += .5f;
+		Debug.Log ("count: " + count);
+
+		if (sort (vect) == sort (vect2) && sort (vect) != 0) {
+			Debug.Log ("Option 1");
+			if (sort (vect) == 1 || sort (vect) == 3) {
+				double angle1 = Math.Atan (dX1 / dZ1);
+				angle1 = angle1 * (180 / Math.PI);
+				Debug.Log ("angle1: " + angle1);
+
+				double angle2 = Math.Atan (dZ2 / dX2);
+				angle2 = angle2 * (180 / Math.PI);
+				Debug.Log ("angle2: " + angle2);
+		
+				angle = 90 - (angle2 + angle1);
+				return angle;
+			}
+			if (sort (vect) == 2 || sort (vect) == 4) {
+				double angle1 = Math.Atan (dZ1 / dX1);
+				angle1 = angle1 * (180 / Math.PI);
+//				Debug.Log ("angle1: " + angle1);
+
+				double angle2 = Math.Atan (dX2 / dZ2);
+				angle2 = angle2 * (180 / Math.PI);
+//				Debug.Log ("angle2: " + angle2);
+
+				angle = 90 - (angle2 + angle1);
+				return angle;
+			}
+
+		} else if (sort (vect) == 0 || sort (vect2) == 0) {
+			if (sort (vect) != 0) {
+				Debug.Log ("Option2");
+				if (sort (vect2) == 2 || sort (vect2) == 4) {
+					angle = Math.Atan (dX1 / dZ1) * (180 / Math.PI); 
+				} else if (sort (vect2) == 1 || sort (vect2) == 3) {
+					angle = Math.Atan (dZ1 / dX1) * (180 / Math.PI);
+				}
+
+			} else if (sort (vect2) != 0) {
+				Debug.Log ("Option3");
+				if (sort (vect2) == 2 || sort (vect2) == 4) {
+					angle = Math.Atan (dZ1 / dX1); 
+				} else if (sort (vect2) == 1 || sort (vect2) == 3) {
+					angle = Math.Atan (dX1 / dZ1) * (180 / Math.PI);
+				}
+				return angle;
+			}
+		
+		} else if (Math.Abs (sort (vect2) - sort (vect)) == 1) { 
+			double angle1;
+			double angle2;
+
+			Debug.Log ("Option3");
+			if (sort (vect) == 1 || sort (vect) == 3) {
+				angle1 = Math.Atan (dZ1 / dX1) * (180 / Math.PI);
+				angle2 = Math.Atan (dZ2 / dX2) * (180 / Math.PI);
+				angle = angle1 + angle2;					
+				return angle;
+			} else if (sort (vect) == 2 || sort (vect) == 4) {
+				angle1 = Math.Atan (dX1 / dZ1) * (180 / Math.PI);
+				angle2 = Math.Atan (dX2 / dZ2) * (180 / Math.PI);
+				angle = angle1 + angle2;
+				return angle;
+			}
+
+		} else if (Math.Abs (sort (vect2) - sort (vect)) == 2) {
+			double angle1;
+			double angle2;
+			Debug.Log ("Option4");
+			if (sort (vect) + sort (vect2) == 4) {
+				angle1 = Math.Atan (dZ1 / dX1) * (180 / Math.PI); 
+				angle2 = Math.Atan (dX2 / dZ2) * (180 / Math.PI);
+				angle = angle1 + angle2 + 90;
+				return angle;
+			}
+
+			if (sort (vect) + sort (vect2) == 6) {
+				angle1 = Math.Atan (dX1 / dZ1) * (180 / Math.PI); 
+				angle2 = Math.Atan (dZ2 / dX2) * (180 / Math.PI);
+				angle = angle1 + angle2 + 90;
+				return angle; 
+			}
+		
+		} else if (sort (vect) == 0 && sort (vect2) == 0) {
+			Debug.Log ("Option5");
+			if (dX1 == 0 && dZ1 > 0) {
+				if (dZ2 == 0 && dX2 > 0)
+					return 90;
+				if (dX2 == 0 && dZ2 < 0)
+					return 180;
+				if (dZ2 == 0 && dX2 < 0)
+					return 270;
+			} else if (dZ1 == 0 && dX1 > 0) {
+				if (dX2 == 0 && dZ2 < 0)
+					return 90;
+				if (dZ2 == 0 && dX2 < 0)
+					return 180;
+				if (dX2 == 0 && dZ2 > 0)
+					return 270;
+			} else if (dX1 == 0 && dZ1 < 0) {
+				if (dZ2 == 0 && dX2 < 0)
+					return 90;
+				if (dX2 == 0 && dZ2 > 0)
+					return 180;
+				if (dZ2 == 0 && dX2 > 0)
+					return 270;
+			} else if (dZ1 == 0 && dX1 < 0) {
+				if (dX2 == 0 && dZ2 > 0)
+					return 90;
+				if (dZ2 == 0 && dX2 > 0)
+					return 180;
+				if (dX2 == 0 && dZ2 < 0)
+					return 270;
+			}
+		}
+		Debug.Log ("sort(vect): " + sort (vect));
+		Debug.Log ("sort(vect2): " + sort (vect2));
+		return -1;
+	}
+		
+}
+
+
---
+++ Rotating Room Study Gain/Assets/CubeController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	public Transform block1;
+	public Transform block2; 
+	public Transform block3;
+	public Transform block4;
+	public Transform block5;
+	public Transform cubes; 
+	public string rearrange;
+	public Transform room;
+
+	public GameObject RigWalls; 
+
+	private int trial = 0; 
+
+	// Use this for initialization
+	void Start () {
+		rearrange = "return";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(rearrange))
+		{
+			if (trial == 0) {
+				block1.localPosition = new Vector3 (.704f, .2f, -.134f);
+				block2.localPosition = new Vector3 (-1.051f, .15f, .298f);
+				block3.localPosition = new Vector3 (-.16f, .125f, -.49f);
+				block4.localPosition = new Vector3 (.047f, .1f, .51f);
+				block5.localPosition = new Vector3 (-.718f, .075f, -.435f);
+				cubes.rotation = room.rotation;
+
+				trial++;
+
+			}else if (trial == 1) {
+				block1.position = new Vector3 (.323f, .6f, -.597f);
+				block4.position = new Vector3 (-.6231f, .6f, -.1678f);
+				block3.position = new Vector3 (-.353f, .5f, .188f);
+				block2.position = new Vector3 (.551f, .5f, .264f);
+				block5.position = new Vector3 (-.568f, .5f, .49f);
+				cubes.rotation = room.rotation;
+
+				trial++;
+
+			} else if (trial == 2) {
+				block1.localPosition = new Vector3 (-.781f, .2f, -.19f);
+				block2.localPosition = new Vector3 (.767f, .15f, -.054f);
+				block3.localPosition = new Vector3 (.412f, .125f, .366f);
+				block4.localPosition = new Vector3 (-.38f, .1f, .355f);
+				block5.localPosition = new Vector3 (.017f, .075f, -.785f);
+				cubes.rotation = room.rotation;
+
+				trial++;
+			} 
+		}
+	}
+
+	public int getTrial() {
+		return trial; 
+	}
+
+}
